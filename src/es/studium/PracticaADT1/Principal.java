24916fc411b5529648176bc11a4ed0dc956125d7
JavPueEsc <javierpueyoescalante@gmail.com>
2024-10-28 13:07:58 +0100

    Ver 4. menú principal implementado. falta implementar consultas

--- src/es/studium/PracticaADT1/Principal.java
+++ src/es/studium/PracticaADT1/Principal.java
@@ -18,9 +18,10 @@ public class Principal {
 	private static ArticuloAComprar articulo;
 
 	public static void main(String[] args) {
-		
+
 		Scanner teclado = new Scanner(System.in);
 		boolean esOpcionValida;
+		char opcionUsuarioMenuPrincipal;
 		char opcionUsuario;
 		char opcionUsuarioSobreescribir = ' ';
 		String nombreLista = "";
@@ -30,142 +31,163 @@ public class Principal {
 
 		listaDeLaCompra = new ListaDeLaCompra();
 
-		System.out.print("Introduzca un nombre para la nueva lista de la compra: ");
-		nombreLista = teclado.nextLine();
-
-		listaDeLaCompra.setNombreLista(nombreLista);
-		System.out.println("El nombre de la lista es: *** " + listaDeLaCompra.getNombreLista() + " ***");
-
+		System.out.println("*** BIENVENID@ A LA APLICACIÓN PARA GESTIONAR TICKETS DE LA COMPRA ***");
+		System.out.println(" ");
+		System.out.println("*** MENÚ PRINCIPAL ***");
 		System.out.println("-------------------------------------------------------------------------");
-		System.out.println(
-				"Si desea anadir un artículo a la lista pulse 'A'.\nSi desea eliminar un artículo de la lista pulse 'E'.\nPara guardar la lista y salir de la aplicación pulse 'G'. ");
+		System.out.println("Para consultar una lista creada pulse 'L'.\nPara crear una neuva lista pulse 'C'");
 		System.out.print("Opción escogida: ");
-		opcionUsuario = teclado.nextLine().toUpperCase().charAt(0);
-		System.out.println("-------------------------------------------------------------------------");
+		opcionUsuarioMenuPrincipal = teclado.nextLine().toUpperCase().charAt(0);
 		esOpcionValida = false;
-		opcionUsuario = validacionOpcionUsuario(teclado, esOpcionValida, opcionUsuario);
-		while (opcionUsuario != 'G') {
-			if (opcionUsuario == 'A') {
-				System.out.println("*** Ha escogido: AÑADIR ARTÍCULO ***");
-				System.out.print("Escriba el artículo que desea añadir a la lista: ");
-				String descripcion = teclado.nextLine();
-				int cantidad = 0;
-				boolean cantidadValida = false;
-				while (!cantidadValida) {
-					try {
-						System.out.print("Escriba la cantidad del artículo que desee comprar: ");
-						cantidad = teclado.nextInt();
-						cantidadValida = true;
-					} catch (InputMismatchException e) {
-						System.err.println("Error: Por favor, ingrese un número entero válido.");
-					}
-					teclado.nextLine();
-				}
-				System.out.print("¿En que formato se presenta dicho artículo? ");
-				String unidad = teclado.nextLine();
+		opcionUsuarioMenuPrincipal = validacionOpcionUsuarioMenuPrincipal(teclado, esOpcionValida,
+				opcionUsuarioMenuPrincipal);
+		System.out.println("-------------------------------------------------------------------------");
+		if (opcionUsuarioMenuPrincipal == 'L') {
+			System.out.println("Consultar");
+		}
 
-				articulo = new ArticuloAComprar(descripcion, cantidad, unidad);
-				listaDeLaCompra.agregarProductoAComprar(articulo);
-				System.out.println(" ");
-				System.out.println("Se ha añadido " + articulo.getDescripcion() + " a la lista de la compra.");
-				System.out.println("La lista de la compra ha quedado de la siguiente manera:");
-				mostrarLista(listaDeLaCompra);
-				System.out.println(" ");
-				System.out.println("-------------------------------------------------------------------------");
-				System.out.println(
-						"Si desea anadir otro artículo a la lista pulse 'A'.\nSi desea eliminar un artículo de la lista pulse 'E'.\nPara mostrar la lista y salir de la aplicación pulse 'G'. ");
-				System.out.print("Opción escogida: ");
-				opcionUsuario = teclado.nextLine().toUpperCase().charAt(0);
-				opcionUsuario = validacionOpcionUsuario(teclado, esOpcionValida, opcionUsuario);
-				System.out.println("-------------------------------------------------------------------------");
-			}
-			if (opcionUsuario == 'E') {
-				System.out.println("*** Ha escogido: ELIMINAR ARTÍCULO ***");
-				if (listaDeLaCompra.listaArticulos.isEmpty()) {
+		if (opcionUsuarioMenuPrincipal == 'C') {
+			System.out.print("Introduzca un nombre para la nueva lista de la compra: ");
+			nombreLista = teclado.nextLine();
+
+			listaDeLaCompra.setNombreLista(nombreLista);
+			System.out.println("El nombre de la lista es: *** " + listaDeLaCompra.getNombreLista() + " ***");
+			System.out.println(" ");
+			System.out.println("*** MENÚ CREACIÓN DE NUEVAS LISTAS ***");
+			System.out.println("-------------------------------------------------------------------------");
+			System.out.println(
+					"Si desea anadir un artículo a la lista pulse 'A'.\nSi desea eliminar un artículo de la lista pulse 'E'.\nPara guardar la lista y salir de la aplicación pulse 'G'. ");
+			System.out.print("Opción escogida: ");
+			opcionUsuario = teclado.nextLine().toUpperCase().charAt(0);
+			System.out.println("-------------------------------------------------------------------------");
+			esOpcionValida = false;
+			opcionUsuario = validacionOpcionUsuario(teclado, esOpcionValida, opcionUsuario);
+			while (opcionUsuario != 'G') {
+				if (opcionUsuario == 'A') {
+					System.out.println("*** Ha escogido: AÑADIR ARTÍCULO ***");
+					System.out.print("Escriba el artículo que desea añadir a la lista: ");
+					String descripcion = teclado.nextLine();
+					int cantidad = 0;
+					boolean cantidadValida = false;
+					while (!cantidadValida) {
+						try {
+							System.out.print("Escriba la cantidad del artículo que desee comprar: ");
+							cantidad = teclado.nextInt();
+							cantidadValida = true;
+						} catch (InputMismatchException e) {
+							System.err.println("Error: Por favor, ingrese un número entero válido.");
+						}
+						teclado.nextLine();
+					}
+					System.out.print("¿En que formato se presenta dicho artículo? ");
+					String unidad = teclado.nextLine();
+
+					articulo = new ArticuloAComprar(descripcion, cantidad, unidad);
+					listaDeLaCompra.agregarProductoAComprar(articulo);
 					System.out.println(" ");
-					System.out.println("No es posible eliminar ningún artículo porque la lista está vacía.");
-					System.out.println("¿Desea realizar alguna otra acción?");
+					System.out.println("Se ha añadido " + articulo.getDescripcion() + " a la lista de la compra.");
+					System.out.println("La lista de la compra ha quedado de la siguiente manera:");
+					mostrarLista(listaDeLaCompra);
 					System.out.println(" ");
+					System.out.println("*** MENÚ CREACIÓN DE NUEVAS LISTAS ***");
 					System.out.println("-------------------------------------------------------------------------");
 					System.out.println(
 							"Si desea anadir otro artículo a la lista pulse 'A'.\nSi desea eliminar un artículo de la lista pulse 'E'.\nPara mostrar la lista y salir de la aplicación pulse 'G'. ");
 					System.out.print("Opción escogida: ");
 					opcionUsuario = teclado.nextLine().toUpperCase().charAt(0);
-					validacionOpcionUsuario(teclado, esOpcionValida, opcionUsuario);
+					opcionUsuario = validacionOpcionUsuario(teclado, esOpcionValida, opcionUsuario);
 					System.out.println("-------------------------------------------------------------------------");
-				} else {
-					System.out.println("Escoja el número del artículo que desea eliminar");
-					int j = 0;
+				}
+				if (opcionUsuario == 'E') {
+					System.out.println("*** Ha escogido: ELIMINAR ARTÍCULO ***");
+					if (listaDeLaCompra.listaArticulos.isEmpty()) {
+						System.out.println(" ");
+						System.out.println("No es posible eliminar ningún artículo porque la lista está vacía.");
+						System.out.println("¿Desea realizar alguna otra acción?");
+						System.out.println(" ");
+						System.out.println("-------------------------------------------------------------------------");
+						System.out.println(
+								"Si desea anadir otro artículo a la lista pulse 'A'.\nSi desea eliminar un artículo de la lista pulse 'E'.\nPara mostrar la lista y salir de la aplicación pulse 'G'. ");
+						System.out.print("Opción escogida: ");
+						opcionUsuario = teclado.nextLine().toUpperCase().charAt(0);
+						validacionOpcionUsuario(teclado, esOpcionValida, opcionUsuario);
+						System.out.println("-------------------------------------------------------------------------");
+					} else {
+						System.out.println("Escoja el número del artículo que desea eliminar");
+						int j = 0;
+						for (ArticuloAComprar articulo : listaDeLaCompra.listaArticulos) {
+							System.out.println(articulo.getDescripcion() + " - " + articulo.getCantidad() + " - "
+									+ articulo.getUnidad() + ". (" + j + ")");
+							j++;
+						}
+						// System.out.println("Valor de j: "+j);
+						System.out.print("¿Qué artículo desea eliminar? ");
+						int articuloEliminado = teclado.nextInt();
+						teclado.nextLine();
+						System.out.println(" ");
+						System.out.println(
+								"Borrando...." + listaDeLaCompra.listaArticulos.get(articuloEliminado).getDescripcion()
+										+ " de la lista de la compra.");
+						listaDeLaCompra.eliminarArticulo(
+								listaDeLaCompra.listaArticulos.get(articuloEliminado).getDescripcion()); // .listaArticulos.remove(articuloEliminado);
+						System.out.println("La lista de la compra ha quedado de la siguiente manera:");
+						mostrarLista(listaDeLaCompra);
+						System.out.println(" ");
+						System.out.println("*** MENÚ CREACIÓN DE NUEVAS LISTAS ***");
+						System.out.println("-------------------------------------------------------------------------");
+						System.out.println(
+								"Si desea anadir un artículo a la lista pulse 'A'.\nSi desea eliminar otro artículo de la lista pulse 'E'.\nPara mostrar la lista y salir de la aplicación pulse 'G'. ");
+						System.out.print("Opción escogida: ");
+						opcionUsuario = teclado.nextLine().toUpperCase().charAt(0);
+						validacionOpcionUsuario(teclado, esOpcionValida, opcionUsuario);
+						System.out.println("-------------------------------------------------------------------------");
+					}
+				}
+			}
+			if (opcionUsuario == 'G') {
+
+				File coincidencia;
+				do {
+					coincidencia = new File("Listas de la compra\\" + listaDeLaCompra.getNombreLista() + ".txt");
+
+					if (coincidencia.exists()) {
+						System.out.println("Ya existe un archivo con ese nombre");
+						System.out.print("¿Desea sobreescribirlo? S/N ");
+						opcionUsuarioSobreescribir = teclado.nextLine().toUpperCase().charAt(0);
+						opcionUsuarioSobreescribir = validacionOpcionUsuarioSobreescribir(teclado, esOpcionValida,
+								opcionUsuarioSobreescribir);
+
+						if (opcionUsuarioSobreescribir == 'N') {
+							System.out.print("Introduzca un nuevo nombre para la lista de la compra: ");
+							nombreLista = teclado.nextLine();
+
+							listaDeLaCompra.setNombreLista(nombreLista);
+						}
+					}
+				} while (coincidencia.exists() && opcionUsuarioSobreescribir == 'N');
+
+				try {
+					FileWriter fw = new FileWriter("Listas de la compra\\" + listaDeLaCompra.getNombreLista() + ".txt",
+							false);
+					BufferedWriter bw = new BufferedWriter(fw);
+					PrintWriter salida = new PrintWriter(bw);
 					for (ArticuloAComprar articulo : listaDeLaCompra.listaArticulos) {
-						System.out.println(articulo.getDescripcion() + " - " + articulo.getCantidad() + " - "
-								+ articulo.getUnidad() + ". (" + j + ")");
-						j++;
+						salida.println(articulo.getDescripcion() + " - " + articulo.getCantidad() + " - "
+								+ articulo.getUnidad() + ".");
 					}
-					// System.out.println("Valor de j: "+j);
-					System.out.print("¿Qué artículo desea eliminar? ");
-					int articuloEliminado = teclado.nextInt();
-					teclado.nextLine();
-					System.out.println(" ");
-					System.out.println(
-							"Borrando...." + listaDeLaCompra.listaArticulos.get(articuloEliminado).getDescripcion()
-									+ " de la lista de la compra.");
-					listaDeLaCompra.eliminarArticulo(listaDeLaCompra.listaArticulos.get(articuloEliminado).getDescripcion()); //.listaArticulos.remove(articuloEliminado);
-					System.out.println("La lista de la compra ha quedado de la siguiente manera:");
-					mostrarLista(listaDeLaCompra);
-					System.out.println(" ");
-					System.out.println("-------------------------------------------------------------------------");
-					System.out.println(
-							"Si desea anadir un artículo a la lista pulse 'A'.\nSi desea eliminar otro artículo de la lista pulse 'E'.\nPara mostrar la lista y salir de la aplicación pulse 'G'. ");
-					System.out.print("Opción escogida: ");
-					opcionUsuario = teclado.nextLine().toUpperCase().charAt(0);
-					validacionOpcionUsuario(teclado, esOpcionValida, opcionUsuario);
-					System.out.println("-------------------------------------------------------------------------");
+					salida.close();
+					bw.close();
+					fw.close();
+					// System.out.println("La lista de la compra
+					// '"+listaDeLaCompra.getNombreLista()+"' se ha guardado correctamente");
+				} catch (IOException k) {
+					System.out.println("Se produjo un error de Archivo");
 				}
+
+				System.out.println("La lista de la compra '" + listaDeLaCompra.getNombreLista()
+						+ "' se ha guardado correctamente");
+				System.exit(0);
 			}
-		}
-		if (opcionUsuario == 'G') {
-
-			File coincidencia;
-			do {
-				coincidencia = new File("Listas de la compra\\" + listaDeLaCompra.getNombreLista() + ".txt");
-
-				if (coincidencia.exists()) {
-					System.out.println("Ya existe un archivo con ese nombre");
-					System.out.print("¿Desea sobreescribirlo? S/N ");
-					opcionUsuarioSobreescribir = teclado.nextLine().toUpperCase().charAt(0);
-					opcionUsuarioSobreescribir = validacionOpcionUsuarioSobreescribir(teclado, esOpcionValida,
-							opcionUsuarioSobreescribir);
-
-					if (opcionUsuarioSobreescribir == 'N') {
-						System.out.print("Introduzca un nuevo nombre para la lista de la compra: ");
-						nombreLista = teclado.nextLine();
-
-						listaDeLaCompra.setNombreLista(nombreLista);
-					}
-				}
-			} while (coincidencia.exists() && opcionUsuarioSobreescribir == 'N');
-
-			try {
-				FileWriter fw = new FileWriter("Listas de la compra\\" + listaDeLaCompra.getNombreLista() + ".txt",
-						false);
-				BufferedWriter bw = new BufferedWriter(fw);
-				PrintWriter salida = new PrintWriter(bw);
-				for (ArticuloAComprar articulo : listaDeLaCompra.listaArticulos) {
-					salida.println(articulo.getDescripcion() + " - " + articulo.getCantidad() + " - "
-							+ articulo.getUnidad() + ".");
-				}
-				salida.close();
-				bw.close();
-				fw.close();
-				// System.out.println("La lista de la compra
-				// '"+listaDeLaCompra.getNombreLista()+"' se ha guardado correctamente");
-			} catch (IOException k) {
-				System.out.println("Se produjo un error de Archivo");
-			}
-
-			System.out.println(
-					"La lista de la compra '" + listaDeLaCompra.getNombreLista() + "' se ha guardado correctamente");
-			System.exit(0);
 		}
 
 	}
@@ -177,7 +199,7 @@ public class Principal {
 			System.out.println("La lista de la compra está vacía");
 		} else {
 			System.out.println("*** " + listaDeLaCompra.getNombreLista() + "***");
-			for (ArticuloAComprar articulo : listaDeLaCompra.listaArticulos) {
+			for (ArticuloAComprar articulo : listaDeLaCompra.getListaArticulos()) {
 				System.out.println(articulo.getDescripcion() + " - " + articulo.getCantidad() + " - "
 						+ articulo.getUnidad() + ".");
 			}
@@ -190,6 +212,8 @@ public class Principal {
 				esOpcionValida = true;
 			} else {
 				System.err.println("La opción escogida no está disponible");
+				System.out.println(" ");
+				System.out.println("*** MENÚ CREACIÓN DE NUEVAS LISTAS ***");
 				System.out.println("-------------------------------------------------------------------------");
 				System.out.println(
 						"Si desea anadir un artículo a la lista pulse 'A'.\nSi desea eliminar un artículo de la lista pulse 'E'.\nPara mostrar la lista y salir de la aplicación pulse 'G'. ");
@@ -216,5 +240,23 @@ public class Principal {
 			}
 		}
 		return opcionUsuarioSobreescribir;
+	}
+
+	private static char validacionOpcionUsuarioMenuPrincipal(Scanner teclado, boolean esOpcionValida,
+			char opcionUsuarioMenuPrincipal) {
+		while (!esOpcionValida) {
+			if (opcionUsuarioMenuPrincipal == 'L' || opcionUsuarioMenuPrincipal == 'C') {
+				esOpcionValida = true;
+			} else {
+				System.err.println("La opción escogida no está disponible");
+				System.out.println("*** MENÚ PRINCIPAL ***");
+				System.out.println("-------------------------------------------------------------------------");
+				System.out.println("Para consultar una lista creada pulse 'L'.\nPara crear una neuva lista pulse 'C'");
+				System.out.print("Opción escogida: ");
+				opcionUsuarioMenuPrincipal = teclado.nextLine().toUpperCase().charAt(0);
+				System.out.println("-------------------------------------------------------------------------");
+			}
+		}
+		return opcionUsuarioMenuPrincipal;
 	}
 }
